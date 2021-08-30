477bf1adef758fecc64c2c5ac820fade021f12e6
AakankshaJha1 <jha.aakanksha111@gmail.com>
2021-08-30 18:14:52 +0530

    Added recent event and countdown

--- js/theme.js
+++ js/theme.js
@@ -11,7 +11,45 @@ $('.page-scroll').bind('click', function(event) {
 });
 
 
+$('#carouselExample').on('slide.bs.carousel', function (e) {
 
+  
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+
+
+
+
+  $(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+
+  });
 ////////////////////////////////////////////////////////////////////////
 // On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
 ////////////////////////////////////////////////////////////////////////
